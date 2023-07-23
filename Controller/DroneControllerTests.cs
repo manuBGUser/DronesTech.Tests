21c05757458413666522288ccfd6a62722efca8d
manuBGUser <parabitcoin97@gmail.com>
2023-07-24 01:32:48 +0200

    The other ones test methods were created

--- Controller/DroneControllerTests.cs
+++ Controller/DroneControllerTests.cs
@@ -8,6 +8,7 @@ using DronesTech.Controllers;
 using DronesTech.DTO;
 using DronesTech.Interfaces;
 using DronesTech.Models;
+using DronesTech.Models.Types;
 using FakeItEasy;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
@@ -44,6 +45,22 @@ namespace DronesTech.Tests.Controller
             result.Should().BeOfType(typeof(OkObjectResult));
         }
 
+        public void DroneController_GetAbledDrones_ReturnOk()
+        {
+            //Arrange
+            var drones = A.Fake<ICollection<DroneDTO>>();
+            var dronesList = A.Fake<List<DroneDTO>>();
+            A.CallTo(() => _mapper.Map<List<DroneDTO>>(drones)).Returns(dronesList);
+            var droneController = new DroneController(_droneRepository, _mapper, _medicineRepository);
+
+            //Act
+            var result = droneController.GetAbledDrones();
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType(typeof(OkObjectResult));
+        }
+
         [Fact]
         public void DroneController_CreateDrone_ReturnOk()
         {
@@ -53,17 +70,87 @@ namespace DronesTech.Tests.Controller
             var droneCreate = A.Fake<DroneDTO>();
             var drones = A.Fake<ICollection<DroneDTO>>();
             var dronesList = A.Fake<List<DroneDTO>>();
-            A.CallTo(() => _droneRepository.GetDrones().Where(d => d.SerieNumber == droneCreate.SerieNumber).FirstOrDefault()).Returns(drone);
+
+            A.CallTo(() => _droneRepository.GetDroneBySerieNumber(droneCreate.SerieNumber)).Returns(drone);
             A.CallTo(() => _mapper.Map<Drone>(droneCreate)).Returns(drone);
+            drone.SerieNumber = "98656";
+            drone.BatteryCapacity = 100;
+            drone.Model = ModelType.HeavyWeight;
+            drone.Status = StatusType.Inactive;
+            drone.WeightLimit = 100;
             A.CallTo(() => _droneRepository.CreateDrone(drone)).Returns(true);
             var droneController = new DroneController(_droneRepository, _mapper, _medicineRepository);
 
             //Act
+            droneMap.SerieNumber = "98656";
+            droneMap.BatteryCapacity = 100;
+            droneMap.Model = 4;
+            droneMap.Status = 1;
+            droneMap.WeightLimit = 100;
             var result = droneController.CreateDrone(droneMap);
 
             //Assert
             result.Should().NotBeNull();
             result.Should().BeOfType(typeof(OkObjectResult));
+        }
+
+        [Fact]
+        public void DroneController_GetDroneBattery_ReturnOk()
+        {
+            //Arrange
+            var droneId = 1;
+            var drone = A.Fake<Drone>();
+            A.CallTo(() => _droneRepository.DroneExists(droneId)).Returns(true);
+            A.CallTo(() => _droneRepository.GetDroneById(droneId)).Returns(drone);
+            var droneController = new DroneController(_droneRepository, _mapper, _medicineRepository);
+
+            //Act
+            var result = droneController.GetDroneBattery(droneId);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType(typeof(OkObjectResult));
+        }
+
+        [Fact]
+        public void DroneController_ChargeMedicineToDrone_ReturnOk()
+        {
+            //Arrange
+            var droneId = 1;
+            var drone = A.Fake<Drone>();
+            var medicines = A.Fake<ICollection<Medicine>>();
+            A.CallTo(() => _droneRepository.DroneExists(droneId)).Returns(true);
+            A.CallTo(() => _droneRepository.IsDroneEmpty(droneId)).Returns(true);
+            A.CallTo(() => _droneRepository.GetDroneById(droneId)).Returns(drone);
+            A.CallTo(() => _droneRepository.GetAbledDrones().Contains(drone)).Returns(true);
+            A.CallTo(() => _medicineRepository.GetMedicinesToCharge(drone.WeightLimit)).Returns(medicines);
+            A.CallTo(() => _droneRepository.ChargeMedicines(drone, medicines)).Returns(drone);
+            var droneController = new DroneController(_droneRepository, _mapper, _medicineRepository);
+
+            //Act
+            var result = droneController.ChargeMedicineToDrone(droneId);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType(typeof(BadRequestObjectResult));
+        }
+
+        [Fact]
+        public void DroneController_CheckChargedMedsWeight_ReturnOk()
+        {
+            //Arrange
+            var droneId = 1;
+            var drone = A.Fake<Drone>();
+            A.CallTo(() => _droneRepository.DroneExists(droneId)).Returns(true);
+            A.CallTo(() => _droneRepository.GetDroneById(droneId)).Returns(drone);
+            var droneController = new DroneController(_droneRepository, _mapper, _medicineRepository);
+
+            //Act
+            var result = droneController.CheckChargedMedsWeight(droneId);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType(typeof(BadRequestObjectResult));
         }
     }
 }
