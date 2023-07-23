1ba969fd0b4383cf6156b3ae6cb04ac16c36dbcb
manuBGUser <parabitcoin97@gmail.com>
2023-07-23 23:06:03 +0200

    First commit. Unit test proect created

---
+++ Controller/DroneControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DronesTech.Tests.Controller
+{
+    internal class DroneControllerTests
+    {
+        public DroneControllerTests()
+        {
+            //_droneRepositories = A.Fake
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace DronesTech.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
