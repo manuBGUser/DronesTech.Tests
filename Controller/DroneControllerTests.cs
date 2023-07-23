9f534dc03e3bc35c39449b92f14c2482a66f798f
manuBGUser <parabitcoin97@gmail.com>
2023-07-23 23:48:06 +0200

    First test created for GetDrones method especiffically

--- Controller/DroneControllerTests.cs
+++ Controller/DroneControllerTests.cs
@@ -3,14 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using DronesTech.Controllers;
+using DronesTech.DTO;
+using DronesTech.Interfaces;
+using FakeItEasy;
+using FluentAssertions;
 
 namespace DronesTech.Tests.Controller
 {
-    internal class DroneControllerTests
+    public class DroneControllerTests
     {
+        public readonly IDroneRepository _droneRepository;
+        private readonly IMapper _mapper;
+        private readonly IMedicineRepository _medicineRepository;
+
         public DroneControllerTests()
         {
-            //_droneRepositories = A.Fake
+            _droneRepository = A.Fake<IDroneRepository>();
+            _mapper = A.Fake<IMapper>();
+            _medicineRepository = A.Fake<IMedicineRepository>();
+        }
+
+        [Fact]
+        public void DroneController_GetDrones_ReturnOk()
+        {
+            //Arrange
+            var drones = A.Fake<ICollection<DroneDTO>>();
+            var dronesList = A.Fake<List<DroneDTO>>();
+            A.CallTo(() => _mapper.Map<List<DroneDTO>>(drones)).Returns(dronesList);
+            var droneController = new DroneController(_droneRepository, _mapper, _medicineRepository);
+
+            //Act
+            var result = droneController.GetDrones();
+
+            //Assert
+            result.Should().NotBeNull();
         }
     }
 }
